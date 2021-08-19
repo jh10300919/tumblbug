7fe9cb7b03a86f7a17b0c2fff08c7656e1eb49b5
jh10300919 <dbfud444@naver.com>
2021-08-19 18:42:59 +0900

    1

---
+++ common/common.js
@@ -0,0 +1,20 @@
+$(() => {
+    //메인 슬라이드
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+})
